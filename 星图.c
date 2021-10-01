17482664cd562ac1c0d08f2d3d835e594dfa1616
Oceanzhouhaiyang <2392982636@qq.com>
2021-10-01 15:28:36 +0800

    星图

---
+++ 星图.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+
+int main()
+{
+	int line = 0;
+	scanf("%d",&line);
+	int i = 0;
+	int j = 0;
+	for (i = 0; i <= line; i++)
+	{
+		for (j = 0; j <line-i ; j++)
+		{
+			printf(" ");
+		}
+		for (j = 0; j<2*i+1; j++)
+		{
+			printf("*");
+		}printf("\n");
+	}
+	return 0;
+}
